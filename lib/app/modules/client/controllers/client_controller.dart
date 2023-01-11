00a9daeeb875dd2f4192040a67ac048a62187da5
Kofi Agyemang <ebensakyi@gmail.com>
2023-01-11 11:21:14 +0000

    Update

--- lib/app/modules/client/controllers/client_controller.dart
+++ lib/app/modules/client/controllers/client_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:icesspool_mobilev2/app/modules/client/views/about_view.dart';
 import 'package:icesspool_mobilev2/app/modules/client/views/settings_view.dart';
@@ -7,10 +8,17 @@ import '../make-request-page/views/make_request_page_view.dart';
 import '../views/make_request_view.dart';
 
 class ClientController extends GetxController {
+  final currentStep = 0.obs;
+  final count = 0.obs;
+  StepperType stepperType = StepperType.vertical;
+
+  final formKey = new GlobalKey<FormState>();
+
+  final selectedRequestType = "".obs;
   final index = 0.obs;
   final screens = [
-    TransactionsView(),
     MakeRequestView(),
+    TransactionsView(),
     SettingsView(),
     AboutView()
   ];
@@ -28,5 +36,28 @@ class ClientController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  currentStepperType() {
+    return stepperType == StepperType.vertical
+        ? stepperType = StepperType.horizontal
+        : stepperType = StepperType.vertical;
+  }
+
+  tapped(int step) {
+    currentStep.value = step;
+  }
+
+  continued() {
+    if (currentStep < 2) {
+      currentStep.value += 1;
+    } else {
+      //   _submitRequest();
+      //  _formSubmitted = true;
+    }
+  }
+
+  cancel() {
+    currentStep.value > 0 ? currentStep.value -= 1 : null;
   }
 }
